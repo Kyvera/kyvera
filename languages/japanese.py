816557e6c918bc916fc6c90560ac9820a671d1e6
Iretiola <abisigairetiola@gmail.com>
2026-03-13 19:33:18 +0100

    Update japanese.py

--- languages/japanese.py
+++ languages/japanese.py
@@ -2,18 +2,21 @@ KEYWORDS = {
     # Romaji
     "hensuu": "VAR",
     "shutsuryoku": "PRINT",
-    "moshi": "IF",
-    "kansu": "FUNCTION",
-    "modoru": "RETURN",
+    "nyuuryoku": "INPUT",     # added for input()
     "moshi": "IF",
     "soredehanai": "ELSE",
+    "kansu": "FUNCTION",
+    "modoru": "RETURN",
 
     # Kanji
     "変数": "VAR",
     "出力": "PRINT",
+    "入力": "INPUT",           # added for input()
     "もし": "IF",
+    "それではない": "ELSE",
     "関数": "FUNCTION",
     "戻る": "RETURN",
+
     "言語": "LANGUAGE",
 }
 
